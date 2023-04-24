f7bc70d26dd1db456b3beffc48e0542dbd35dfe8
Marcin Chrost <mchrost82@gmail.com>
2023-04-24 15:24:31 +0200

    Autoclose application context

--- src/main/java/pl/wojtyna/trainings/spring/SpringTrainingV2Application.java
+++ src/main/java/pl/wojtyna/trainings/spring/SpringTrainingV2Application.java
@@ -5,17 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringTrainingV2Application {
 
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext();
-        context.register(CliAdapter.class,
-                         InvestorService.class,
-                         SurnameAwareCliCommandsMapper.class,
-                         InMemoryInvestorRepository.class,
-                         RestClientInvestorProfileService.class,
-                         UnirestClient.class,
-                         HardcodedRestResources.class);
-        context.refresh();
-        var cliAdapter = context.getBean(CliAdapter.class);
-        cliAdapter.run(args);
+        try (var context = new AnnotationConfigApplicationContext()) {
+            context.register(CliAdapter.class,
+                    InvestorService.class,
+                    SurnameAwareCliCommandsMapper.class,
+                    InMemoryInvestorRepository.class,
+                    RestClientInvestorProfileService.class,
+                    UnirestClient.class,
+                    HardcodedRestResources.class);
+            context.refresh();
+            var cliAdapter = context.getBean(CliAdapter.class);
+            cliAdapter.run(args);
+        }
     }
 
 }
