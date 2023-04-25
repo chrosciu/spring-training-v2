49cc45ed4a95c0db0eea57b6b5c1d0646e1c4934
Marcin Chrost <mchrost82@gmail.com>
2023-04-25 16:18:37 +0200

    Task 11 - exception handler in controller

--- src/main/java/pl/wojtyna/trainings/spring/crowdsorcery/investor/rest/InvestorRestApi.java
+++ src/main/java/pl/wojtyna/trainings/spring/crowdsorcery/investor/rest/InvestorRestApi.java
@@ -3,6 +3,7 @@ package pl.wojtyna.trainings.spring.crowdsorcery.investor.rest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,18 +29,12 @@ public class InvestorRestApi {
     }
 
     @PostMapping
-    public ResponseEntity<RegisterInvestorErrorResponse> register(@RequestBody RegisterInvestorRestDto registerInvestorRestDto) {
-        try {
-            investorService.register(new RegisterInvestor(registerInvestorRestDto.id(),
-                                                          registerInvestorRestDto.name()));
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body(new RegisterInvestorErrorResponse(registerInvestorRestDto, e.getMessage()));
-        }
+    public ResponseEntity<Void> register(@RequestBody RegisterInvestorRestDto registerInvestorRestDto) {
+        investorService.register(new RegisterInvestor(registerInvestorRestDto.id(),
+                registerInvestorRestDto.name()));
         return ResponseEntity.created(URI.create("/investorModule/api/v0/investors/%s".formatted(
-                                 registerInvestorRestDto.id())))
-                             .build();
+                        registerInvestorRestDto.id())))
+                .build();
     }
 
     @GetMapping("/{id}")
@@ -59,9 +54,6 @@ public class InvestorRestApi {
                                                             exception.getMessage()));
     }
 
-    private record RegisterInvestorErrorResponse(RegisterInvestorRestDto command, String reason) {
-
-    }
 
     private record GenericErrorResponse(String status, String reason) {}
 }
