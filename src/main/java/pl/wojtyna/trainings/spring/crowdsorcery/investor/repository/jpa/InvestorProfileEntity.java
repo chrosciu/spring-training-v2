157b7ee7d9351425a0c57b373f06ffa5483fd3ea
Marcin Chrost <mchrost82@gmail.com>
2023-04-26 15:16:29 +0200

    Task 17 - fixed @ToString in one-to-one relationship

--- src/main/java/pl/wojtyna/trainings/spring/crowdsorcery/investor/repository/jpa/InvestorProfileEntity.java
+++ src/main/java/pl/wojtyna/trainings/spring/crowdsorcery/investor/repository/jpa/InvestorProfileEntity.java
@@ -2,6 +2,7 @@ package pl.wojtyna.trainings.spring.crowdsorcery.investor.repository.jpa;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.OneToOne;
 @Entity
 @Data
 @EqualsAndHashCode(of = "id")
+@ToString(exclude = "investor")
 public class InvestorProfileEntity {
 
     @Id
